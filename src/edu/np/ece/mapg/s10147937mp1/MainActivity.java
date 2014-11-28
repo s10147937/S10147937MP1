0709e454f7692eb9c4ff3b86732088ae7517f236
s10147937 <s10147934@connect.np.edu.sg>
2014-11-28 15:33:12 +0800

    Final Version
    shows total score

---
+++ src/edu/np/ece/mapg/s10147937mp1/MainActivity.java
@@ -0,0 +1,75 @@
+package edu.np.ece.mapg.s10147937mp1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	int myNumber = 7;
+	int totalScore = 0;
+	EditText etNumber;
+	Button btGuess;
+	TextView tvGuess;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		etNumber = (EditText) this.findViewById(R.id.etNumber);
+		btGuess = (Button) this.findViewById(R.id.btGuess);
+		tvGuess = (TextView) this.findViewById(R.id.tvGuess);
+
+		btGuess.setOnClickListener(buttonListener);
+	}
+
+	private View.OnClickListener buttonListener = new View.OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			try {
+				int num = Integer.parseInt(etNumber.getText().toString());
+
+				if (num == myNumber) {
+					Toast.makeText(MainActivity.this, "Bingo! Correct number.",
+							Toast.LENGTH_LONG).show();
+
+					totalScore++;
+					StringBuilder sb = new StringBuilder();
+					sb.append("Your Score: " + totalScore);
+
+					tvGuess.setText(sb.toString());
+				} else
+					Toast.makeText(MainActivity.this, "Try Again!",
+							Toast.LENGTH_SHORT).show();
+			} catch (Exception e) {
+				System.out.print("Please enter a number from 0 to 9");
+			}
+		}
+	};
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
